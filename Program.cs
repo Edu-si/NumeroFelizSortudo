12fe04d0b3bc25735b2b3d9dfffe3c3e9e55b07e
Edu-si <edu.isys@gmail.com>
2019-03-23 15:49:41 -0300

    Add files via upload
    *** Números Felizes ***
    
    Um número é considerado feliz, se em algum ponto a soma de seus dígitos ao quadrado equivale a 1.
    
    Ex: 7 é um número feliz? 
    7² = 49 
    4² + 9² = 97 
    9² + 7² = 130 
    1² + 3² + 0² = 10 
    1² + 0² = 1 
    
    Logo, 7 é um número feliz. No seu programa você deve considerar um máximo de 100 iterações.
    
    *** Números Sortudos ***
    
    Nós começamos com uma lista de inteiros começando em 1: 
    
    1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
    
    Então removemos todos os números com posição múltipla de 2 (todos números pares), deixando todos os inteiros ímpares: 
    
    1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25
    
    O segundo termo desta sequência é 3. 
    Nós removemos então todos os números com posição múltipla de 3 que sobraram na lista: 
    
    1, 3, 7, 9, 13, 15, 19, 21, 25
    
    O terceiro termo desta sequência é 7. 
    Nós removemos então todos os números com posição múltipla de 7 que sobraram na lista: 1, 3, 7, 9, 13, 15, 21, 25
    
    Se nós repetirmos este procedimento indefinidamente, os sobreviventes são os números sortudos:
    
    1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99
    
    Casos de Teste
    1) 7 – Número Sortudo e Feliz.
    2) 21 – Número Sortudo e Não-Feliz.
    3) 28 – Número Não-Sortudo e Feliz.
    4) 142 – Número Não-Sortudo e Não-Feliz
    5) 37 – Número Sortudo e Não-Feliz
    6) 100 – Número Não-Sortudo e Feliz.

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InicioPrograma();
+            Console.ReadKey();
+        }
+
+
+        private static void InicioPrograma(){
+            
+            string entrada;
+
+            Console.WriteLine("Digite um número");
+            entrada = Console.ReadLine();
+            try 
+        	{	        
+		            bool IsSortudo = VerificarNumeroSortudo(entrada);
+                    string mensagem = entrada + " - ";
+               
+                     if (IsSortudo)
+                        mensagem += "Número Sortudo e ";
+                     else
+                        mensagem += "Número Não-Sortudo e ";
+             
+                     bool Isfeliz = VerificarNumeroFeliz(entrada);
+                     if (Isfeliz)
+                        mensagem += "Feliz.";
+                     else
+                        mensagem += "Não-Feliz.";
+
+                    Console.WriteLine(mensagem);
+                    InicioPrograma();
+
+                }
+	            catch (Exception)
+	            {
+                    Console.WriteLine("Digite um número valido");
+                    InicioPrograma();
+	            }
+        }
+
+        private static bool VerificarNumeroFeliz(string entrada)
+        {
+            double valorFinal = 0;
+            for (int i = 0; i < 100; i++)
+            {
+
+                //Calculo dos valores
+                for (int j = 0; j < entrada.Length; j++)
+                {
+                    double somaValores = Math.Pow(Convert.ToDouble(entrada[j].ToString()), 2);
+                    valorFinal += somaValores;
+                }
+
+                if (valorFinal == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    entrada = valorFinal.ToString();
+                    valorFinal = 0;
+                }
+
+            }
+            return false;
+        }
+
+
+        private static bool VerificarNumeroSortudo(string entrada)
+        {
+            List<int> lista = new List<int>();
+
+            for (int i = 1; i <= Convert.ToInt32(entrada); i++)
+            {
+                lista.Add(i);
+            }
+
+            //posição múltipla de 2 
+            int posicao = 1;
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                if (i == posicao)
+                {
+                    lista.RemoveAt(posicao);
+                    posicao = posicao + 1;
+                }
+            }
+
+            //posição múltipla de 3 
+            posicao = 2;
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                if (i == posicao)
+                {
+                    lista.RemoveAt(posicao);
+                    posicao = posicao + 2;
+                }
+            }
+
+            //posição múltipla de 7
+            posicao = 6;
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                if (i == posicao)
+                {
+                    lista.RemoveAt(posicao);
+                    posicao = posicao + 6;
+                }
+            }
+
+            var achou = lista.FindAll(x => x == Convert.ToInt32(entrada));
+
+            return achou.Count == 1 ? true : false;
+
+        }
+
+    }
+}
